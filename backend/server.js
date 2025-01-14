35b2b249e77a148c92e4e0bf6e02ff677c2da996
sangam5756 <sangammunde3@gmail.com>
2025-01-14 06:54:12 +0530

    callback url changed

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import passport from "passport";
 import session from "express-session";
 import path, { dirname } from "path";
 
-import "./passport/github.auth.js"
+import "./passport/github.auth.js";
 
 import userRoute from "./routes/user.route.js";
 import exploreRoute from "./routes/explore.route.js";
@@ -15,41 +15,41 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
-
-const corsOptions = { 
-  origin:  ["http://localhost:5173", process.env.FRONTEND_URL, "https://gitcode.sangammundhe.site"], // Allow requests from this origin
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL,
+    "https://gitcode.sangammundhe.site",
+  ], // Allow requests from this origin
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
- 
+
 const app = express();
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 // console.log("__dirname", __dirname)
 
-
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 
 app.get("/", (req, res) => {
   res.send("Server is Ready");
 });
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoute);
-app.use("/api/explore",exploreRoute);
-
+app.use("/api/explore", exploreRoute);
 
 // app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 // app.get("*", (req, res) => {
 // 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 // });
-
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
